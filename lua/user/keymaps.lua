0ebd635969885c6de15166f4f84d6e1442ed9fce
Cedric Wider <wider.cedric@gmail.com>
2022-04-04 11:58:01 +0200

    Use Alt to navigate in tmux/nvim

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -20,12 +20,6 @@ vim.g.maplocalleader = ";"
 -- No Highlight
 keymap("n", "<C-n>", ":nohl<cr>", opts)
 
--- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
-
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
@@ -33,8 +27,8 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<C-l>", ":bnext<CR>", opts)
+keymap("n", "<C-h>", ":bprevious<CR>", opts)
 
 -- Insert --
 -- Press jk fast to enter
