a4385611c114115ddecfd49316c0ab9441d1aa68
Cedric Wider <wider.cedric@gmail.com>
2022-04-08 21:06:38 +0200

    Auto-format on save using LSP

---
+++ lua/user/lsp/settings/solargraph.lua
@@ -0,0 +1,11 @@
+return {
+	cmd = { "solargraph", "stdio" },
+	filetypes = { "ruby" },
+	init_options = { formatting = true },
+	settings = {
+		solargraph = {
+			completion = true,
+			diagnostis = true,
+		},
+	},
+}
