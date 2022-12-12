c8ac22f4cbab89d238408c06915d911a08dbcf8b
Cedric Wider <wider.cedric@gmail.com>
2022-12-12 17:22:10 +0100

    Disable transparency by default
    It's not looking so great when the terminal has transparency enabled.

--- lua/user/transparent.lua
+++ lua/user/transparent.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 transparent.setup({
-	enable = true, -- boolean: enable transparent
+	enable = false, -- boolean: enable transparent
 	extra_groups = { -- table/string: additional groups that should be cleared
 		-- In particular, when you set it to 'all', that means all available groups
 
